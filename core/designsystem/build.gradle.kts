708f40acbe0e12c6dec400f5265be504915286fb
Saeed <mr.ms.moradi@gmail.com>
2022-11-30 22:37:18 +0330

    Create core design system module

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("blackbank.android.library")
+    id("blackbank.android.library.compose")
+}
+
+android {
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    namespace = "com.saeed.blackbank.core.designsystem"
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation.layout)
+    api(libs.androidx.compose.material.iconsExtended)
+    api(libs.androidx.compose.material3)
+    debugApi(libs.androidx.compose.ui.tooling)
+    api(libs.androidx.compose.ui.tooling.preview)
+    api(libs.androidx.compose.ui.util)
+    api(libs.androidx.compose.runtime)
+}
