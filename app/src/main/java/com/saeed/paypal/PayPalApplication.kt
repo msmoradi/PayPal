d80b6a8d06e9caf8139a97838397646c30f2bcb2
Saeed <mr.ms.moradi@gmail.com>
2022-12-02 20:50:15 +0330

    Add application class

---
+++ app/src/main/java/com/saeed/paypal/PayPalApplication.kt
@@ -0,0 +1,10 @@
+package com.saeed.paypal
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * [Application] class for PayPal
+ */
+@HiltAndroidApp
+class PayPalApplication : Application()
