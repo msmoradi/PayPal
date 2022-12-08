06ff9d3663f7c49d324cba2c6614e4d71617c325
Saeed <mr.ms.moradi@gmail.com>
2022-12-08 22:12:46 +0330

    Create model classes

---
+++ core/model/src/main/java/com/saeed/paypal/model/ContactModel.kt
@@ -0,0 +1,25 @@
+package com.saeed.paypal.model
+
+data class ContactModel(
+    val name: String,
+    val email: String
+)
+
+val previewContacts = listOf(
+    ContactModel(
+        name = "Andrew Dillan",
+        email = "andrew.dillan@gmail.com"
+    ),
+    ContactModel(
+        name = "Alex Millton",
+        email = "alxmillton@yahoo.com"
+    ),
+    ContactModel(
+        name = "Don Norman",
+        email = "don.norman@gmail.com"
+    ),
+    ContactModel(
+        name = "Mike Rine",
+        email = "mikerine@gmail.com"
+    )
+)
