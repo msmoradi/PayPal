cbf027df3fc6ad267f6a2507307bb677c89be580
Saeed <mr.ms.moradi@gmail.com>
2023-01-12 16:52:47 +0330

    Refactor ui functions

--- feature/home/src/main/java/com/saeed/paypal/feature/home/composables/HomeAppBar.kt
+++ feature/home/src/main/java/com/saeed/paypal/feature/home/composables/HomeAppBar.kt
@@ -1,21 +1,27 @@
 package com.saeed.paypal.feature.home.composables
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Check
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.saeed.paypal.core.designsystem.theme.PayPalTheme
 import com.saeed.paypal.core.ui.DevicePreviews
+import com.saeed.paypal.feature.home.R
+import com.saeed.paypal.core.designsystem.R as DR
 
 @Composable
 fun HomeAppBar(
@@ -23,15 +29,30 @@ fun HomeAppBar(
 ) {
     Column(
         modifier
+            .aspectRatio(1.3786f)
+            .paint(
+                painter = painterResource(id = R.drawable.frame_header_background),
+                contentScale = ContentScale.FillWidth
+            )
+            .padding(horizontal = 32.dp, vertical = 16.dp)
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .paddingFromBaseline(top = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(imageVector = Icons.Rounded.Check, contentDescription = null)
-            Icon(imageVector = Icons.Rounded.Close, contentDescription = null)
+            Image(
+                modifier = Modifier.size(36.dp),
+                painter = painterResource(id = DR.drawable.ic_paypal_logo),
+                contentDescription = null
+            )
+            Image(
+                modifier = Modifier.size(52.dp),
+                painter = painterResource(id = R.drawable.profile_13),
+                contentDescription = null
+            )
         }
 
         Text(
--- feature/home/src/main/java/com/saeed/paypal/feature/home/composables/HomeContent.kt
+++ feature/home/src/main/java/com/saeed/paypal/feature/home/composables/HomeContent.kt
@@ -1,15 +1,17 @@
 package com.saeed.paypal.feature.home.composables
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,54 +28,63 @@ fun HomeContent(
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
-        Box(
-            modifier = modifier
-                .fillMaxWidth()
-                .background(color = MaterialTheme.colorScheme.primary)
-                .padding(32.dp)
+        HomeAppBar()
+        LazyColumn(
+            modifier = modifier,
+            contentPadding = PaddingValues(horizontal = 32.dp)
         ) {
-            HomeAppBar()
+
+            item {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier
+                        .padding(vertical = 24.dp)
+                        .fillMaxWidth()
+                        .height(140.dp)
+                ) {
+                    ActionItem(
+                        modifier = Modifier.weight(3f),
+                        iconResId = R.drawable.ic_send_money,
+                        text = "Send Money",
+                        contentColor = MaterialTheme.colorScheme.background,
+                        backgroundColors = listOf(
+                            Color(0xFF0070BA), MaterialTheme.colorScheme.primary
+                        ),
+                        onItemClicked = {}
+                    )
+                    ActionItem(
+                        modifier = Modifier.weight(3f),
+                        iconResId = R.drawable.ic_request_money,
+                        text = "Request Money",
+                        contentColor = MaterialTheme.colorScheme.primary,
+                        backgroundColors = listOf(
+                            MaterialTheme.colorScheme.background,
+                            MaterialTheme.colorScheme.background
+                        ),
+                        onItemClicked = {}
+                    )
+                    MoreActionItem(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight()
+                    )
+                }
+            }
+
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Activity")
+                    Text(text = "View All")
+                }
+            }
+
+            items(items = activities) {
+                ActivityRow(activityModel = it)
+            }
         }
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier
-                .padding(horizontal = 32.dp, vertical = 24.dp)
-                .fillMaxWidth()
-                .height(140.dp)
-        ) {
-            ActionItem(
-                modifier = Modifier.weight(3f),
-                iconResId = R.drawable.ic_send_money,
-                text = "Send Money",
-                contentColor = MaterialTheme.colorScheme.background,
-                backgroundColors = listOf(
-                    Color(0xFF0070BA), MaterialTheme.colorScheme.primary
-                ),
-                onItemClicked = {}
-            )
-            ActionItem(
-                modifier = Modifier.weight(3f),
-                iconResId = R.drawable.ic_request_money,
-                text = "Request Money",
-                contentColor = MaterialTheme.colorScheme.primary,
-                backgroundColors = listOf(
-                    MaterialTheme.colorScheme.background,
-                    MaterialTheme.colorScheme.background
-                ),
-                onItemClicked = {}
-            )
-            MoreActionItem(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-            )
-        }
-        ActivitySection(
-            activities,
-            modifier = Modifier
-                .padding(horizontal = 32.dp, vertical = 24.dp)
-                .fillMaxWidth()
-        )
     }
 }
 
