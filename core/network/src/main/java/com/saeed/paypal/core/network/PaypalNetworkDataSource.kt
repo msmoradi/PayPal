d93a71954f4d8514a87027db83ffe0f6241e2cba
Saeed <mr.ms.moradi@gmail.com>
2023-01-15 00:42:35 +0330

    Add contact repository to PaypalNetworkDataSource.kt

--- core/network/src/main/java/com/saeed/paypal/core/network/PaypalNetworkDataSource.kt
+++ core/network/src/main/java/com/saeed/paypal/core/network/PaypalNetworkDataSource.kt
@@ -1,8 +1,11 @@
 package com.saeed.paypal.core.network
 
 import com.saeed.paypal.core.network.model.NetworkActivity
+import com.saeed.paypal.core.network.model.NetworkContact
 
 interface PaypalNetworkDataSource {
 
     suspend fun getActivities(): List<NetworkActivity>
+
+    suspend fun getContacts(): List<NetworkContact>
 }
